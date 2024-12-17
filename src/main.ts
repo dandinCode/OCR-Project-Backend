1c98891668a3365411229ed39783be1836662ccb
Caio <dandinzin@gmail.com>
2024-12-17 11:35:10 -0300

    corrigindo cors

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
   app.enableCors({
-    origin: 'http://localhost:3000',  
+    origin: process.env.FRONT_URL,  
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
     credentials: true,
   });
 
